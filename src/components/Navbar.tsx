408a2625ad43cb840dff53fec0a5c4fe21735f2f
franvaccani <franciscovaccani08@gmail.com>
2025-02-20 17:01:44 -0300

    animacion nav

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -12,17 +12,41 @@ const Navbar = () => {
           <Link to="/" className="flex items-center space-x-2">
             <img className='w-28' src={logo} alt="" />
           </Link>
-          
+
           {/* Desktop Menu */}
           <div className="hidden md:flex items-center space-x-8">
-            <Link to="/" className=" transition">Inicio</Link>
-            <Link to="/productos" className=" transition">Productos</Link>
-            <Link to="/sobre-nosotros" className=" transition">Nosotros</Link>
-            <Link to="/contacto" className="transition">Contacto</Link>
+            <Link
+              to="/"
+              className="pb-[2px] hover:text-gray-300 relative group transition"
+            >
+              Inicio
+              <span className="absolute left-0 bottom-0 w-0 h-[1px] bg-gray-300 transition-all duration-500 group-hover:w-full"></span>
+            </Link>
+            <Link
+              to="/productos"
+              className="pb-[2px] hover:text-gray-300 relative group transition"
+            >
+              Productos
+              <span className="absolute left-0 bottom-0 w-0 h-[1px] bg-gray-300 transition-all duration-500 group-hover:w-full"></span>
+            </Link>
+            <Link
+              to="/sobre-nosotros"
+              className="pb-[2px] hover:text-gray-300 relative group transition"
+            >
+              Nosotros
+              <span className="absolute left-0 bottom-0 w-0 h-[1px] bg-gray-300 transition-all duration-500 group-hover:w-full"></span>
+            </Link>
+            <Link
+              to="/contacto"
+              className="pb-[2px] hover:text-gray-300 relative group transition"
+            >
+              Contacto
+              <span className="absolute left-0 bottom-0 w-0 h-[1px] bg-gray-300 transition-all duration-500 group-hover:w-full"></span>
+            </Link>
           </div>
 
           {/* Mobile Menu Button */}
-          <button 
+          <button
             className="md:hidden"
             onClick={() => setIsOpen(!isOpen)}
           >
@@ -35,29 +59,29 @@ const Navbar = () => {
       {isOpen && (
         <div className="md:hidden">
           <div className="px-2 pt-2 pb-3 space-y-1">
-            <Link 
-              to="/" 
+            <Link
+              to="/"
               className="block px-3 py-2 hover:bg-black rounded-md"
               onClick={() => setIsOpen(false)}
             >
               Inicio
             </Link>
-            <Link 
-              to="/productos" 
+            <Link
+              to="/productos"
               className="block px-3 py-2 hover:bg-black rounded-md"
               onClick={() => setIsOpen(false)}
             >
               Productos
             </Link>
-            <Link 
-              to="/sobre-nosotros" 
+            <Link
+              to="/sobre-nosotros"
               className="block px-3 py-2 hover:bg-black rounded-md"
               onClick={() => setIsOpen(false)}
             >
               Nosotros
             </Link>
-            <Link 
-              to="/contacto" 
+            <Link
+              to="/contacto"
               className="block px-3 py-2 hover:bg-black rounded-md"
               onClick={() => setIsOpen(false)}
             >
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -58,7 +58,7 @@ const Home = () => {
       <section className="py-16 bg-gray-50">
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
           <h2 className="text-3xl font-bold text-gray-900 mb-8">Camisetas Destacadas</h2>
-          <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
+          <div className="grid grid-cols-1 md:grid-cols-3 mb-10 gap-8">
             {featuredProducts.map((product) => (
               <Link 
                 key={product.id}
